b1dfb41d5197925f4dfad1d3938df78b77a5fe9c
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2024-05-22 20:32:02 +0200

    still bloqué

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useContext } from "react";
-import axios from "axios";
-import "./searchBar.css"
-import { DataContext } from "../../../context/data.context"
+import "./searchBar.css";
+import { DataContext } from "../../../context/data.context";
 
 const SearchBar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
-  const { places } = useContext(DataContext)
+  const { places } = useContext(DataContext);
 
-  const handleSearch = async (event) => {
+  const handleSearch = (event) => {
     event.preventDefault();
-    if(searchQuery.trim().length === 0){
-        setSearchQuery(places)
+    if (searchQuery.toString().trim().length === 0) {
+      onSearch(places);
+    } else {
+      try {
+        const results = places.filter((place) =>
+          place.properties.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        onSearch(results);
+      } catch (error) {
+        console.error(error);
+        onSearch([]);
+      }
     }
-    const response = await axios.get(`/api/search?query=${searchQuery}`);
-    onSearch(response.data);
   };
 
   return (
     <div>
       <form onSubmit={handleSearch}>
         <input
-        className="input-search"
+          className="input-search"
           type="text"
           value={searchQuery}
           onChange={(event) => setSearchQuery(event.target.value)}
         />
-        <button className="btn-search" type="submit">Rechercher</button>
+        <button className="btn-search" type="submit">
+          Rechercher
+        </button>
       </form>
     </div>
   );
