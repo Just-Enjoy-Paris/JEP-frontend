1b535b984b6d7da7ff48133bff09ac8b4404b20d
batmah2 <mahmoudisml3@gmail.com>
2024-05-22 15:49:39 +0200

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,23 @@ import Places from "./pages/places/Places.jsx"
 import Events from "./pages/events/Events.jsx"
 import Contact from "./pages/contact/Contact.jsx"
 import Error from "./pages/error/Error.jsx"
-
 import Signup from "./pages/signup/Signup.jsx"
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/log" element={<Log />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/places" element={<Places />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
- {/*<Footer />*/}
+      <Header />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/log" element={<Log />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/places" element={<Places />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      {/*<Footer />*/}
     </Router>
   )
 }
