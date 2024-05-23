20df484baa23a872d1c0bff302dd808071af05fc
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2024-05-23 20:22:23 +0200

    searchbar and filter work indépendament

--- src/components/PlaceList/PlaceList.jsx
+++ src/components/PlaceList/PlaceList.jsx
@@ -24,6 +24,17 @@ export default function PlaceList() {
     selectedCategories.includes(place.properties.category[0])
   );
 
+  // Filtrer les lieux en fonction des catégories sélectionnées et du terme de recherche
+  // const filteredPlaces =
+  // selectedCategories.length === 0 && !searchResult
+  //   ? places
+  //   : selectedCategories.length === 0 && searchResult
+  //   ? searchResult
+  //   : places.filter((place) =>
+  //       selectedCategories.includes(place.properties.category[0]) &&
+  //       new RegExp(searchResult, "i").test(place.properties.name)
+  //     );
+
   return (
     <section className="placeList">
       <h1 className="placeListTitle">Lieux</h1>
