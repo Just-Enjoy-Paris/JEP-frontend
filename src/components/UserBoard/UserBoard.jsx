1b5bb35f2b9646980937e2205e0576768dacc032
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2024-05-27 13:28:49 +0200

    new package for css added

--- src/components/UserBoard/UserBoard.jsx
+++ src/components/UserBoard/UserBoard.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import classNames from "classnames"
+import "./userBoard.css"
 
 const UserBoard = () => {
   const navigate = useNavigate()
