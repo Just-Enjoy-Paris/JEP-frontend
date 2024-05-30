48c4f30cbe8349241c30b86a28d4407b2d051c12
stfDS <dossantos.stefane@gmail.com>
2024-05-30 16:29:03 +0200

    fix avatar , modif update user/2

--- src/components/UserBoard/UserBoard.jsx
+++ src/components/UserBoard/UserBoard.jsx
@@ -3,21 +3,26 @@ import axios from "axios"
 import React, { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../../../context/user.context"
-
 import "./userBoard.css"
 
 const UserBoard = () => {
   const navigate = useNavigate()
   const { isAuthenticated, update, setUpdate, user } = useContext(AuthContext)
-  const [newAvatar, setNewAvatar] = useState(null)
-  const [newEmail, setNewEmail] = useState(null)
+  const [newEmail, setNewEmail] = useState()
+  const [file, setFile] = useState(null)
 
   const updateUser = async e => {
     e.preventDefault()
+    const formData = new FormData()
+    formData.append("email", newEmail)
+    if (file) {
+      formData.append("avatar", file)
+    }
+
     try {
       await axios.put(
         `${import.meta.env.VITE_API_URL}/updateprofile`,
-        { email: newEmail, avatar: newAvatar },
+        formData,
         { withCredentials: true }
       )
       setUpdate(!update)
@@ -33,30 +38,40 @@ const UserBoard = () => {
   }, [isAuthenticated, navigate])
 
   return (
-    <div className="espace-client">
+    <section className="userBoard">
       <h1 className="title">Mes informations</h1>
       <form onSubmit={updateUser}>
-        <label>
-          Email
-          <input
-            type="email"
-            placeholder={user.email}
-            value={newEmail}
-            onChange={e => setNewEmail(e.target.value)}
-          />
-        </label>
-
-        <label htmlFor="newAvatar" className="avatarArea">
-          Avatar
-          <input
-            type="file"
-            value={newAvatar}
-            onChange={e => setNewAvatar(e.target.value)}
-          />
-        </label>
-        <button onClick={updateUser}>Enregistrer</button>
+        <div>
+          <label>
+            Email
+            <input
+              type="email"
+              placeholder={user ? user.email : "email"}
+              value={newEmail}
+              onChange={e => setNewEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="newAvatar" className="avatarArea">
+            Avatar
+            {/* à retirer aprés ajout du loader */}
+            {user && (
+              <div className="userBoardAvatar">
+                <img src={user.account.avatar} alt="user avatar" />
+              </div>
+            )}
+            {file && (
+              <div className="previewAvatar">
+                <img src={URL.createObjectURL(file)} alt="Nouvel avatar" />
+              </div>
+            )}
+            <input type="file" onChange={e => setFile(e.target.files[0])} />
+          </label>
+        </div>
+        <button type="submit">Enregistrer</button>
       </form>
-    </div>
+    </section>
   )
 }
 
