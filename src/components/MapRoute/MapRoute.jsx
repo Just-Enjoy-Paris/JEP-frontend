e7aa60b5de7ad436869d2c2f49661d2087a197cf
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2024-06-11 16:14:49 +0200

    map with route is good

--- src/components/MapRoute/MapRoute.jsx
+++ src/components/MapRoute/MapRoute.jsx
@@ -1,65 +1,79 @@
-import { useEffect, useRef, useContext } from "react"
+import { useContext, useEffect } from "react"
 import { DataContext } from "../../../context/data.context"
 import AdComponent from "../AdComponent/AdComponent"
 
-export default function MapRoute({ map }) {
-  const mapRef = useRef(null)
-  const directionsRendererRef = useRef(null)
+export default function MapRoute() {
   const { places } = useContext(DataContext)
 
   useEffect(() => {
-    if (!map || !window.google) return
+    const initMap = () => {
+      const googleMaps = window.google.maps
 
-    mapRef.current = new window.google.maps.Map(document.getElementById("map"), {
-      center: { lat: 48.8566, lng: 2.3522 },
-      zoom: 12,
-      mapId: "fe2668b01b9cb7be"
-    })
+      const map = new googleMaps.Map(document.getElementById("map"), {
+        center: { lat: 48.8566, lng: 2.3522 },
+        zoom: 12,
+        mapId: "fe2668b01b9cb7be",
+      })
 
-    // DirectionsRenderer pour afficher l'itinéraire sur la carte
-    directionsRendererRef.current = new window.google.maps.DirectionsRenderer({
-      map: mapRef.current,
-    })
+      const directionsService = new googleMaps.DirectionsService()
+      const directionsRenderer = new googleMaps.DirectionsRenderer({ map })
 
-    const limitedPlaces = places.slice(0, 25)
+      // Limiter les lieux à 25
+      const limitedPlaces = places.slice(0, 25)
 
-    const directionsService = new window.google.maps.DirectionsService();
-
-    const waypoints = limitedPlaces.slice(1, -1).map((place) => ({
-      location: {
-        lat: place.geometry.coordinates[1],
-        lng: place.geometry.coordinates[0],
-      },
-      stopover: true,
-    }))
-
-    directionsService.route(
-      {
-        origin: {
-          lat: limitedPlaces[0].geometry.coordinates[1],
-          lng: limitedPlaces[0].geometry.coordinates[0],
+      const waypoints = limitedPlaces.slice(1, -1).map((place) => ({
+        location: {
+          lat: place.geometry.coordinates[1],
+          lng: place.geometry.coordinates[0],
         },
-        destination: {
-          lat: limitedPlaces[limitedPlaces.length - 1].geometry.coordinates[1],
-          lng: limitedPlaces[limitedPlaces.length - 1].geometry.coordinates[0],
+        stopover: true,
+      }))
+
+      directionsService.route(
+        {
+          origin: {
+            lat: limitedPlaces[0].geometry.coordinates[1],
+            lng: limitedPlaces[0].geometry.coordinates[0],
+          },
+          destination: {
+            lat: limitedPlaces[limitedPlaces.length - 1].geometry.coordinates[1],
+            lng: limitedPlaces[limitedPlaces.length - 1].geometry.coordinates[0],
+          },
+          waypoints: waypoints,
+          travelMode: "WALKING",
         },
-        waypoints: waypoints,
-        travelMode: "WALKING",
-      },
-      (result, status) => {
-        if (status === "OK") {
-          directionsRendererRef.current.setDirections(result)
-        } else {
-          console.error("Échec du calcul d'itinéraire : ", status)
+        (result, status) => {
+          if (status === "OK") {
+            directionsRenderer.setDirections(result)
+          } else {
+            console.error("Échec du calcul d'itinéraire : ", status)
+          }
+        }
+      )
+    }
+
+    const loadGoogleMaps = async () => {
+      if (!document.getElementById("google-maps-script")) {
+        const script = document.createElement("script")
+        script.id = "google-maps-script"
+        script.src = `${import.meta.env.VITE_API_URL_AND_KEYMAP}`
+        script.async = true
+        script.defer = true
+        document.body.appendChild(script)
+        script.onload = initMap
+      } else {
+        if (window.google && window.google.maps) {
+          initMap()
         }
       }
-    )
-
-  }, [map, places])
+    }
+    loadGoogleMaps()
+  }, [places])
 
   return (
     <section>
       <div id="map" className="mapContainer"></div>
-      <AdComponent/>
-    </section>  )
+      <AdComponent />
+    </section>
+  )
 }
