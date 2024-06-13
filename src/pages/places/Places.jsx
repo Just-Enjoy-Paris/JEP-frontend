1680967b1c673a40cdfbae0937cacd71fa9b6c7b
batmah2 <mahmoudisml3@gmail.com>
2024-06-13 17:57:15 +0200

    add new style and logic for displaying the card in the places page

--- src/pages/places/Places.jsx
+++ src/pages/places/Places.jsx
@@ -6,12 +6,59 @@ import { useState } from "react"
 export default function Places() {
   const [searchResult, setSearchResult] = useState(null)
 
+  const categoryMap = {
+    Restaurant: [
+      "Restaurant",
+      "Fast food",
+      "Streetfood Chinoise",
+      "Restaurant Algerien",
+      "Restaurant Italien"
+    ],
+    Shopping: [
+      "Boutique",
+      "Bijouterie",
+      "Association",
+      "Boutique de vêtements", 
+      "Boutique de porcelaine"
+    ],
+    Gourmandise: [
+      "Pâtisserie",
+      "Bubble tea",
+      "Salon de thé",
+      "Marchand de glaces"
+    ],
+    Courses: ["Boulangerie", "Primeur", "Marché"],
+    Hôtel: ["Hotel"],
+    Culture: ["Boutique d'objets religieux", "Librairie"],
+    Pratique: ["Coiffeur", "Boutique de CBD", "Vente et réparation de cycles"]
+  }
+
+  const categorySections = [
+    { categories: ["Restaurant"] },
+    { categories: ["Gourmandise"] },
+    { categories: ["Shopping"] },
+    { categories: ["Pratique"] },
+    { categories: ["Courses"] },
+    { categories: ["Hôtel"] },
+    { categories: ["Culture"] }
+  ]
+
   return (
     <main className="placesPage">
       <h1 className="placeListTitle">Lieux</h1>
       <SearchBar setSearchResult={setSearchResult} />
-      <section className="placeList">
-        <PlaceList searchResult={searchResult} />
+      <section className="placeListContainer">
+        {categorySections.map(({ title, categories }) => (
+          <div className="category-section" key={title}>
+            {categories.map(category => (
+              <PlaceList
+                key={category}
+                searchResult={searchResult}
+                categoryMap={{ [category]: categoryMap[category] }}
+              />
+            ))}
+          </div>
+        ))}
       </section>
     </main>
   )
