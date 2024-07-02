d138db82f4cfed821944c49b3aa6de7d2cef362a
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2024-07-02 19:40:10 +0200

    methodes created : getter and setter cookie

--- src/components/CookieConsent/CookieConsent.jsx
+++ src/components/CookieConsent/CookieConsent.jsx
@@ -3,10 +3,32 @@ import Cookies from "js-cookie"
 
 import "./cookieConsent.css"
 
-// Fonction personnalisée pour vérifier le consentement aux cookies
+// Customized function to verify cookie consent
 const getCookieConsent = () => {
   const cookieConsent = Cookies.get("cookie_consent")
   return cookieConsent === "true"
+}
+
+// Function to store user geolocation in a cookie
+const setGeolocationCookie = (latitude, longitude) => {
+  Cookies.set("geolocation", JSON.stringify({ latitude, longitude }), {
+    expires: 30 // 30 days
+  })
+}
+
+// Function to store the user's advertising clicks in a cookie
+const setAdClickCookie = adId => {
+  const adClicks = Cookies.getJSON("ad_clicks") || []
+  adClicks.push(adId)
+  Cookies.set("ad_clicks", adClicks, {
+    expires: 30
+  })
+}
+
+// Function to retrieve the user's advertising clicks from a cookie
+const getAdClickCookie = () => {
+  const adClicks = Cookies.getJSON("ad_clicks") || []
+  return adClicks
 }
 
 const CookieConsent = () => {
@@ -18,32 +40,26 @@ const CookieConsent = () => {
 
   const handleAccept = () => {
     setHasConsented(true)
-    Cookies.set("cookie_consent", "true", { expires: 30 }) // 30 days
+    Cookies.set("cookie_consent", "true", { expires: 30 })
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      setGeolocationCookie(latitude, longitude)
+      const adClicks = getAdClickCookie()
+      if (adClicks.length > 0) {
+        // Display ads similar to the user's previous clicks
+      } else {
+        // Display ads by default
+      }
+    })
   }
 
   const handleRefuse = () => {
     setHasConsented(false)
-    Cookies.set("cookie_consent", "false", { expires: 1 }) // 1 day
+    Cookies.set("cookie_consent", "false", { expires: 1 })
   }
 
   if (hasConsented) {
     return null
-  }
-
-  // Function to store user geolocation in a cookie
-  const setGeolocationCookie = (latitude, longitude) => {
-    Cookies.set("geolocation", JSON.stringify({ latitude, longitude }), {
-      expires: 30 // 30 jours
-    })
-  }
-
-  // Function to store the user's advertising clicks in a cookie
-  const setAdClickCookie = adId => {
-    const adClicks = Cookies.getJSON("ad_clicks") || []
-    adClicks.push(adId)
-    Cookies.set("ad_clicks", adClicks, {
-      expires: 30 // 30 jours
-    })
   }
 
   return (
@@ -60,4 +76,9 @@ const CookieConsent = () => {
 }
 
 export default CookieConsent
-export { getCookieConsent }
+export {
+  getCookieConsent,
+  setGeolocationCookie,
+  setAdClickCookie,
+  getAdClickCookie
+}
